42c589d51f4757910f19fbbf09c90aeb3a7f2699
Ruslan <gren.ruslan@gmail.com>
2023-05-06 23:03:13 +0300

    final stage

--- src/Converter.java
+++ src/Converter.java
@@ -1,4 +1,6 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Converter {
@@ -25,13 +27,21 @@ public class Converter {
             if (input.equals("/back")) {
                 break;
             }
-            BigInteger num = decimalNumberSystem(input);
-            System.out.println("Conversion result: " + toNumberSystem(num));
+            System.out.print("Conversion result: ");
+            if (input.contains(".")) {
+                String[] twoParts = input.split("\\.");
+                BigInteger numInteger = toBigInteger(twoParts[0]);
+                BigDecimal numDecimal = toBigDecimal(twoParts[1]);
+                System.out.println(toNumberSystemInteger(numInteger) + "." + toNumberSystemDecimal(numDecimal));
+            } else {
+                BigInteger num = toBigInteger(input);
+                System.out.println(toNumberSystemInteger(num));
+            }
             System.out.println();
         }
     }
 
-    private static void changeBase() throws IllegalAccessException {
+    private static void changeBase() throws IllegalArgumentException {
         System.out.println("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
         String[] input = scanner.nextLine().split(" ");
         if (input[0].equals("/exit")) {
@@ -39,19 +49,19 @@ public class Converter {
             return;
         }
         if (input.length != 2) {
-            throw new IllegalAccessException("Wrong length");
+            throw new IllegalArgumentException("Wrong length");
         }
         base[0] = Integer.parseInt(input[0]);
         base[1] = Integer.parseInt(input[1]);
         if (base[0] < 2 || base[0] > 36) {
-            throw new IllegalAccessException("Wrong first parameter");
+            throw new IllegalArgumentException("Wrong first parameter");
         }
         if (base[1] < 2 || base[1] > 36) {
-            throw new IllegalAccessException("Wrong second parameter");
+            throw new IllegalArgumentException("Wrong second parameter");
         }
     }
 
-    private static String toNumberSystem(BigInteger num) {
+    private static String toNumberSystemInteger(BigInteger num) {
         StringBuilder output = new StringBuilder();
         BigInteger numberSystem = BigInteger.valueOf(base[1]);
         while (numberSystem.compareTo(num)  <= 0) {
@@ -61,10 +71,32 @@ public class Converter {
         return output.insert(0, array.charAt(num.intValue())).toString();
     }
 
-    private static BigInteger decimalNumberSystem(String num) {
+    private static String toNumberSystemDecimal(BigDecimal num) {
+        StringBuilder output = new StringBuilder();
+        BigDecimal numberSystem = BigDecimal.valueOf(base[1]);
+        int i = 0;
+        while (i != 5) {
+            num = num.multiply(numberSystem);
+            int wholePart = num.divide(BigDecimal.ONE, RoundingMode.UNNECESSARY).intValue();
+            output.append(array.charAt(wholePart));
+            num = num.subtract(BigDecimal.valueOf(wholePart));
+            i++;
+        }
+        return output.toString();
+    }
+
+    private static BigInteger toBigInteger(String num) {
         BigInteger output = BigInteger.ZERO;
         for (int i = 0; i < num.length(); i++) {
             output = output.add(BigInteger.valueOf((long) (array.indexOf(num.charAt(i)) * (Math.pow(base[0], num.length()-i-1)))));
+        }
+        return output;
+    }
+
+    private static BigDecimal toBigDecimal(String num) {
+        BigDecimal output = BigDecimal.ZERO;
+        for (int i = -1; i >= -num.length(); i--) {
+            output = output.add(BigDecimal.valueOf((array.indexOf(num.charAt(-i-1)) * (Math.pow(base[0], i)))));
         }
         return output;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package converter;
-
 public class Main {
     public static void main(String[] args) {
         Converter.start();
